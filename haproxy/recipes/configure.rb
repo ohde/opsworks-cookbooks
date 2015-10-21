26cd7ba89477117fd20c28e0d9d906b32cb36a27
Michael Ohde <maohde@gmail.com>
2015-10-21 07:06:31 -0700

    Download file from s3 and pass to template

--- haproxy/recipes/configure.rb
+++ haproxy/recipes/configure.rb
@@ -3,6 +3,21 @@ service "haproxy" do
   action :nothing # only define so that it can be restarted if the config changed
 end
 
+require 'aws-sdk'
+
+s3 = AWS::S3.new
+
+# Set bucket and object name
+obj = s3.buckets['j6-haproxy-test'].objects['projects.json']
+
+# Read content to variable
+file_content = obj.read
+
+# Log output (optional)
+Chef::Log.info(file_content)
+
+projects = JSON.parse(file_content)
+
 template "/etc/haproxy/haproxy.cfg" do
   cookbook "haproxy"
   source "haproxy.cfg.erb"
@@ -10,6 +25,7 @@ template "/etc/haproxy/haproxy.cfg" do
   group "root"
   mode 0644
   notifies :reload, "service[haproxy]"
+  variables :projects => projects
 end
 
 execute "echo 'checking if HAProxy is not running - if so start it'" do
